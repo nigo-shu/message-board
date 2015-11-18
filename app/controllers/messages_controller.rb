08b1ed191bc7633be5fd3cfc4f6484090a73f3f6
nigo_shu <nigo@oj9.co.jp>
2015-11-18 13:53:57 +0000

    commit2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,25 @@
 class MessagesController < ApplicationController
-  def index
+  before_action :set_message, only: [:edit, :update]
+
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      #　保存に成功した場合はトップページにリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+　　else
+　　  #　保存に失敗した場合は編集面に戻す
+　　  render 'edit'
+　　end
+　end
+　
+　　 def index
     # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
   end
-
+  
   ## ここから追記
   def create
     @message = Message.new(message_params)
@@ -16,6 +31,7 @@ class MessagesController < ApplicationController
       flash.now[:alert] = "メッセージの保存に失敗しました。"
   render 'index'
 end
+
 end
 
 
@@ -23,5 +39,11 @@ end
   def message_params
     params.require(:message).permit(:name, :body)
   end
+  
+  def set_message
+    @message = Message.find(params[:id])
   ## ここまで
+end
+end
+end
 end
