541f62a010f76686451f567f9e8a2b60715035d8
nigo_shu <nigo@oj9.co.jp>
2015-11-23 14:28:12 +0000

    commit challenge2 finish

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112110830) do
+ActiveRecord::Schema.define(version: 20151123131507) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
